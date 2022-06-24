d0bde5c72788f5ee2642e7206dc4c0d4c9061e74
horikawa <nesheep.dream@gmail.com>
2022-06-24 15:19:47 +0900

    add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const App: FC = () => {
   return (
-    <div>
-      WikiLinks
-    </div>
+    <Box>
+      <Typography variant="h3">
+        WikiLinks
+      </Typography>
+    </Box>
   );
 };
 
