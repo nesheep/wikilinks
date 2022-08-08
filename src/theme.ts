c11a409c5b4e55c8fbc12d7436b013bea5ba6b8d
horikawa <nesheep.dream@gmail.com>
2022-08-09 07:13:55 +0900

    clean up

--- src/theme.ts
+++
@@ -1,7 +0,0 @@
-import { createTheme } from '@mui/material/styles';
-
-export const theme = createTheme({
-  typography: {
-    fontFamily: ['Roboto', 'Noto Sans JP', 'Helvetica', 'Arial', 'sans-serif'].join(','),
-  },
-});
